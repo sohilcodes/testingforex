5c423b0e132e926d70c919e45ea992b12ddf6513
Sohil Khan <96345871+sohilcodes@users.noreply.github.com>
2026-06-18 20:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,67 @@
 from flask import Flask
-import threading
-import time
 import requests
 import os
 
 app = Flask(__name__)
 
-API_KEY = os.getenv("API_KEY")
-
-
-def bot():
-    while True:
-        try:
-            url = "https://api.twelvedata.com/time_series"
-
-            params = {
-                "symbol": "EUR/USD",
-                "interval": "5min",
-                "apikey": API_KEY
-            }
-
-            r = requests.get(url)
-
-            print(r.json())
-
-        except Exception as e:
-            print(e)
-
-        time.sleep(60)
+TOKEN = os.getenv("OANDA_API_KEY")
+ACCOUNT = os.getenv("ACCOUNT_ID")
 
 
 @app.route("/")
 def home():
-    return "Forex Algo Bot Running"
+
+    if not TOKEN or not ACCOUNT:
+        return {
+            "status": "missing_env"
+        }
+
+    url = (
+        f"https://api-fxpractice.oanda.com/v3/accounts/{ACCOUNT}"
+    )
+
+    headers = {
+        "Authorization":
+        f"Bearer {TOKEN}"
+    }
+
+    try:
+
+        r = requests.get(
+            url,
+            headers=headers,
+            timeout=20
+        )
+
+        data = r.json()
+
+        if r.status_code == 200:
+
+            account = data["account"]
+
+            return {
+                "connected": True,
+                "currency": account["currency"],
+                "balance": account["balance"]
+            }
+
+        return data
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
 
 
-threading.Thread(target=bot, daemon=True).start()
+if __name__ == "__main__":
 
-port = int(os.getenv("PORT", 10000))
-
-app.run(
-    host="0.0.0.0",
-    port=port
-)
+    app.run(
+        host="0.0.0.0",
+        port=int(
+            os.getenv(
+                "PORT",
+                10000
+            )
+        )
+    )
